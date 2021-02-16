405e4fcddbc82ec143d23be27dc88272508f1d11
yitangsun <53280136+yitangsun@users.noreply.github.com>
2021-02-16 02:39:50 -0500

    Update Transfer_vcf.sh

--- Transfer_vcf.sh
+++ Transfer_vcf.sh
@@ -14,9 +14,38 @@ cd $SLURM_SUBMIT_DIR
 
 module load PLINK/2.00-alpha2.3-x86_64-20200124
 
-chr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X XY)
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 1 --recode vcf --out chr1_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 2 --recode vcf --out chr2_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 3 --recode vcf --out chr3_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 4 --recode vcf --out chr4_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 5 --recode vcf --out chr5_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 6 --recode vcf --out chr6_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 7 --recode vcf --out chr7_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 8 --recode vcf --out chr8_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 9 --recode vcf --out chr9_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 10 --recode vcf --out chr10_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 11 --recode vcf --out chr11_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 12 --recode vcf --out chr12_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 13 --recode vcf --out chr13_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 14 --recode vcf --out chr14_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 15 --recode vcf --out chr15_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 16 --recode vcf --out chr16_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 17 --recode vcf --out chr17_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 18 --recode vcf --out chr18_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 19 --recode vcf --out chr19_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 20 --recode vcf --out chr20_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 21 --recode vcf --out chr21_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr 22 --recode vcf --out chr22_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr X --recode vcf --out chr23_Gibbons_Project.StrandAligned.rsID_Updated
+plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr Y --recode vcf --out chr24_Gibbons_Project.StrandAligned.rsID_Updated
 
-plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr $chr --recode vcf --out chr$chrGibbons_Project.StrandAligned.rsID_Updated
+
+
+
+
+#chr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X XY)
+
+#plink2 --bfile Gibbons_Project.StrandAligned.rsID_Updated --chr $chr --recode vcf --out chr$chrGibbons_Project.StrandAligned.rsID_Updated
 
 
 #cd /project/kylab/lab_shared/CFR_UGA_Sociology/genotype/Strand_Alignment
